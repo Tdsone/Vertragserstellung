8868da73e8c76b34c199fa12e1100c62e3dc07f9
Tdsone <timon_schneider@outlook.de>
2021-03-17 11:53:32 +0100

    authentication against ad

---
+++ Vertragserstellung/auth/auth-config.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = 'ichpissaufdasmic'
+const CLIENT_SECRET = 'undmeinemuckerklingtfetter'
+const TENANT = 'academyconsult.onmicrosoft.com'
+
+export { CLIENT_ID, CLIENT_SECRET, TENANT }
