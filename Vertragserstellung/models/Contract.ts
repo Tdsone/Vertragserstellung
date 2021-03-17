209558007d9565c0dabfd3406db29a014ede1c68
Tdsone <timon_schneider@outlook.de>
2021-03-17 13:18:40 +0100

    vertragstexterstellung function skeleton

--- Vertragserstellung/models/Contract.ts
+++ Vertragserstellung/models/Contract.ts
@@ -1,18 +1,59 @@
+import SharePoint from './Sharepoint'
 export default class Contract {
  
     type : string;
     text : string;
+    sharePoint : SharePoint;
+    templateText : string; 
 
-    constructor(type, text){
+    constructor(type){
         this.type = type; 
-        this.text = text;
+        this.text = "";
+        this.sharePoint = null;
     }
 
-    async createTextWithDefaultValues(){
+    async initialize(){
+        this.sharePoint = new SharePoint()
+        this.sharePoint.initialize() // TODO provide init details
+    }
 
+    async fetchContractTemplateText() : Promise<string>{
+        return ""
+    }
+    
+    async generatePlaceholderValueMap() : Promise<Map<string, string>>{
+        return new Map()
+    }
+
+    async generateContractText(replacePlaceholders){
+        
+        let templateText : string; 
+        let placeholderValueMap : Map<string, string>
+
+        try {
+            
+            let promises : Promise<any>[] = [this.fetchContractTemplateText(), this.generatePlaceholderValueMap()]
+            const data = await Promise.all(promises)
+            
+            templateText = data[0]
+            placeholderValueMap = data[1]
+
+            if(!templateText || !placeholderValueMap){
+                throw new Error("An error occurred fetching default values or template")
+            }    
+        } catch(error){
+            console.error(error)
+        }
+
+        const text = replacePlaceholders(templateText, placeholderValueMap)
+        this.setText(text)
     }
 
     async saveToSharepoint(){
-        
+   
+    }
+
+    setText(text : string){
+        this.text = text
     }
 }
