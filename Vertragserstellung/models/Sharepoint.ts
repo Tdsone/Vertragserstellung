d5a28927c6242d764a53d5328dfd917799cc6cd2
Tdsone <timon_schneider@outlook.de>
2021-03-17 12:22:05 +0100

    init function for GraphAPI

--- Vertragserstellung/models/Sharepoint.ts
+++ Vertragserstellung/models/Sharepoint.ts
@@ -1,10 +1,12 @@
-export default class Sharepoint{
+export default class GraphAPI{
 
     authToken : string;
     graphBaseURL : string; 
+    apiVersion : string; 
 
     constructor(){
-
+        this.authToken = ""
+        this.graphBaseURL = ""
     }
 
     async authenticate(adal, graphBaseURL : string, clientID : string, clientSecret : string, tenant : string){
@@ -27,12 +29,24 @@ export default class Sharepoint{
         this.setAuthToken(authToken)
     }
 
+    async initialize(adal, graphBaseURL = "", clientID : string, clientSecret : string, tenant : string){
+        if(graphBaseURL == ""){
+            return new Error("Could not initialize; graphBaseURL is missing")
+        }
+        this.setGraphBaseURL(graphBaseURL)
+        await this.authenticate(adal, graphBaseURL, clientID, clientSecret, tenant)
+    }
+
     async getSPListItem(){
 
     }
 
     setAuthToken(token){
         this.authToken = token
+    }
+
+    setGraphBaseURL(graphBaseURL: string){
+        this.graphBaseURL = graphBaseURL
     }
 
 }
