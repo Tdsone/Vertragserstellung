4c99ae4ab0874a7850d0d18b64412fe93baea16a
Tdsone <timon_schneider@outlook.de>
2021-03-17 13:52:21 +0100

    require instead of import with jest tests

--- Vertragserstellung/tests/index.test.js
+++ Vertragserstellung/tests/index.test.js
@@ -1,9 +1,9 @@
-import {replacePlaceHolders} from '../utils'
+const utils = require('../utils')
 
 test("Tests if all placeholders are replaced correctly", () => {
     
-    const placeholderValueMap = new Map([["++name++","Rudolf"],["++volume++","12"], ["++kommtnichtvor++","troelf"]])
-    const text = "Der Hahn mit Namen ++name++ schrie mit Lautstärke ++volume++. Allerdings hatte ++name++ einen Brotkrumen im Hals und erstickte in Folge daran."
+    const placeholderValueMap = new Map([["##name##","Rudolf"],["##volume##","12"], ["##kommtnichtvor##","troelf"]])
+    const text = "Der Hahn mit Namen ##name## schrie mit Lautstärke ##volume##. Allerdings hatte ##name## einen Brotkrumen im Hals und erstickte in Folge daran."
     
-    expect(replacePlaceHolders(text, placeholderValueMap)).toBe("Der Hahn mit Namen Rudolf schrie mit Lautstärke 12. Allerdings hatte Rudolf einen Brotkrumen im Hals und erstickte in Folge daran.")
+    expect(utils.replacePlaceHolders(text, placeholderValueMap)).toBe("Der Hahn mit Namen Rudolf schrie mit Lautstärke 12. Allerdings hatte Rudolf einen Brotkrumen im Hals und erstickte in Folge daran.")
 })
