d6f5d354e98c67e270d8f525118146730a94bf93
Tdsone <timon_schneider@outlook.de>
2021-03-17 11:51:26 +0100

    contract class for contract generation

---
+++ Vertragserstellung/models/Contract.ts
@@ -0,0 +1,18 @@
+export default class Contract {
+ 
+    type : string;
+    text : string;
+
+    constructor(type, text){
+        this.type = type; 
+        this.text = text;
+    }
+
+    async createTextWithDefaultValues(){
+
+    }
+
+    async saveToSharepoint(){
+        
+    }
+}
