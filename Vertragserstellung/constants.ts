78e62f0cd7027c5620bd34ebbe4a3ba7d75ede07
Tdsone <timon_schneider@outlook.de>
2021-03-18 15:31:22 +0100

    SP ListIDs and SP Site URL

--- Vertragserstellung/constants.ts
+++ Vertragserstellung/constants.ts
@@ -1,8 +1,14 @@
 // Graph API
-const GRAPH_BASE_URL = 'https://graph.microsoft.com'
-const DEFAULT_SP_WEBSITE = 'database'
+const SP_SITE_URL = 'https://graph.microsoft.com/v1.0/sites/academyconsult.sharepoint.com,b57543b9-c8fa-48ae-9e02-0082785f1c94,f96372d3-b7a1-4d7e-9889-7dfe12692b10/lists/'
+const SP_VERTRAGSVORLAGEN_LIST_ID = "ce80cab7-bfb7-4e59-87fe-551853cef734"
+const SP_VERTRAEGE_LIST_ID = "7e4f7ec7-4a66-41e3-9fc5-71791cf385a5"
+
+// Auth
+const TENANT = 'academyconsult.onmicrosoft.com'
 
 export {
-    GRAPH_BASE_URL, 
-    DEFAULT_SP_WEBSITE
+    SP_SITE_URL,
+    SP_VERTRAGSVORLAGEN_LIST_ID, 
+    SP_VERTRAEGE_LIST_ID,
+    TENANT
 }
