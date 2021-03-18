ce1b75e84d6f458927bc171264ed8f113053e5a2
Tdsone <timon_schneider@outlook.de>
2021-03-18 15:31:48 +0100

    better readability and env var for secret

--- Vertragserstellung/index.ts
+++ Vertragserstellung/index.ts
@@ -4,33 +4,60 @@ import {createDefaultContract} from './main'
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     
-    // check if projectID and contractType are specified
-    if(req.query.projectID && req.query.contractType){
+    if(!req.query.projectID){
+        context.res = {
+            status: 400, 
+            body: `projectID is missing as query parameter.`
+        }
+        context.done()
+    }
 
-        const projectID = req.query.projectID
-        const contractType = req.query.contractType
+    if(!req.query.contractType){
+        context.res = {
+            status: 400, 
+            body: `contractType is missing as query parameter.`
+        }
+        context.done()
+    }
 
-        const {info, error} = await createDefaultContract(projectID, contractType)
+    if(!process.env["CLIENT_ID"]){
+        context.res = {
+            status: 400, 
+            body: `CLIENT_ID is missing as env variable.`
+        }
+        context.done()
+    }
 
-        if(error){
-            context.res = {
-                status: 400,
-                body: {
-                    message: "Something went wrong creating the default contract"
-                }
-            }
-        } else {
-            context.res = {
-                status: 200,
-                body: {
-                    message: "The contract was sucessfully created", 
-                }
+    if(!process.env["CLIENT_SECRET"]){
+        context.res = {
+            status: 400, 
+            body: `CLIENT_SECRET is missing as env variable.`
+        }
+        context.done()
+    }
+    
+    const projectID = req.query.projectID
+    const contractType = req.query.contractType
+    
+    const clientID = process.env["CLIENT_ID"]
+    const clientSecret = process.env["CLIENT_SECRET"]
+
+    const {info, error} = await createDefaultContract(projectID, contractType, clientID, clientSecret)
+
+    if(!error){
+        context.res = {
+            status: 200,
+            body: {
+                message: "The contract was sucessfully created",
+                // TODO pass info
             }
         }
     } else {
         context.res = {
-            status: 400, 
-            body: `One of the following parameters might be missing: projectID, contractType`
+            status: 500,
+            body: {
+                message: "We could not create your contract", 
+            }
         }
     }
     context.done()
